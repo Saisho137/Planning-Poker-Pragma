b4833c2d7ad53c199dba63e8d773765b99cae0b3
Santiago <santib.2001@hotmail.com>
2023-12-06 17:21:04 -0500

    feat: added basic generic input structure and logic

--- planning-poker-pragma/src/app/components/atoms/generic-input/generic-input.component.ts
+++ planning-poker-pragma/src/app/components/atoms/generic-input/generic-input.component.ts
@@ -1,13 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-generic-input',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './generic-input.component.html',
-  styleUrl: './generic-input.component.css'
+  styleUrl: './generic-input.component.css',
 })
 export class GenericInputComponent {
+  @Output() message: EventEmitter<string> = new EventEmitter<string>();
+  string: string = '';
 
+  onInputChange() {
+    this.message.emit(this.string);
+  }
 }
