87099f289d8c2bd0c584471b2bd9e42c10fc7e24
Santiago <santib.2001@hotmail.com>
2023-12-17 22:12:53 -0500

    feat: made scoring mode a object list with id property instead of string list

--- planning-poker-pragma/src/app/services/classrooms.service.ts
+++ planning-poker-pragma/src/app/services/classrooms.service.ts
@@ -11,12 +11,43 @@ export class ClassroomsService {
   private rooms: ClassroomInterface[] = [];
   private users: UserInRoomInterface[] = [];
   private scoringMode = [
-    ['1', '2', '3', '5', '8', '13', '21', '?', '☕'],
-    ['1', '2', '3', '4', '5', '?', '☕'],
-    ['10', '20', '30', '40', '50', '60', '70', '80', '90', '100', '?', '☕'],
+    [
+      { id: 1, value: '1' },
+      { id: 2, value: '2' },
+      { id: 3, value: '3' },
+      { id: 4, value: '5' },
+      { id: 5, value: '8' },
+      { id: 6, value: '13' },
+      { id: 7, value: '21' },
+      { id: 8, value: '?' },
+      { id: 9, value: '☕' },
+    ],
+    [
+      { id: 1, value: '1' },
+      { id: 2, value: '2' },
+      { id: 3, value: '3' },
+      { id: 4, value: '4' },
+      { id: 5, value: '5' },
+      { id: 6, value: '?' },
+      { id: 7, value: '☕' },
+    ],
+    [
+      { id: 1, value: '10' },
+      { id: 2, value: '20' },
+      { id: 3, value: '30' },
+      { id: 4, value: '40' },
+      { id: 5, value: '50' },
+      { id: 6, value: '60' },
+      { id: 7, value: '70' },
+      { id: 8, value: '80' },
+      { id: 9, value: '90' },
+      { id: 10, value: '100' },
+      { id: 11, value: '?' },
+      { id: 12, value: '☕' },
+    ],
   ];
 
-  public createScoringMode(mode: string) {
+  public createScoringMode(mode: string): { id: number; value: string }[] {
     switch (mode) {
       case 'fibonacci':
         return this.scoringMode[0];
@@ -25,7 +56,7 @@ export class ClassroomsService {
       case 'oneHundred':
         return this.scoringMode[2];
     }
-    return ['void'];
+    return [{ id: 0, value: 'null' }];
   }
 
   public createRoom(
