4afb176faf2564dea3c10b80e923670eb2af72ac
Santiago <santib.2001@hotmail.com>
2023-12-21 00:32:17 -0500

    feat: added 'reveal cards' button

--- planning-poker-pragma/src/app/components/molecules/table/table.component.ts
+++ planning-poker-pragma/src/app/components/molecules/table/table.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { GenericButtonComponent } from '../../atoms/generic-button/generic-button.component';
 
 @Component({
   selector: 'app-table',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, GenericButtonComponent],
   templateUrl: './table.component.html',
   styleUrl: './table.component.css'
 })
