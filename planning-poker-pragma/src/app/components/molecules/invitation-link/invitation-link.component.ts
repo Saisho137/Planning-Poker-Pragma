294ee3331fd1a4ce1ca0bc58455a38a981411487
Santiago <santib.2001@hotmail.com>
2024-01-04 12:40:54 -0500

    feat: added copy to clipboard functionality

--- planning-poker-pragma/src/app/components/molecules/invitation-link/invitation-link.component.ts
+++ planning-poker-pragma/src/app/components/molecules/invitation-link/invitation-link.component.ts
@@ -2,6 +2,7 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { GenericButtonComponent } from '../../atoms/generic-button/generic-button.component';
 import { GenericInputComponent } from '../../atoms/generic-input/generic-input.component';
+import { Clipboard } from '@angular/cdk/clipboard';
 
 @Component({
   selector: 'app-invitation-link',
@@ -13,6 +14,13 @@ import { GenericInputComponent } from '../../atoms/generic-input/generic-input.c
 export class InvitationLinkComponent {
   @Input() link: string = '';
   @Output() clickEvent: EventEmitter<void> = new EventEmitter<void>();
+
+  constructor(private clipboard: Clipboard) {}
+
+  copyUrl(): void {
+    this.clipboard.copy(this.link);
+    alert('Copiado con éxito')
+  }
 
   onButtonClick() {
     this.clickEvent.emit();
