dcbb0b30e022f4400529479ced3b5f2c79cfaa22
Santiago <santib.2001@hotmail.com>
2024-01-18 15:32:50 -0500

    feat: Added Dropdown of card menu options

--- planning-poker-pragma/src/app/components/organisms/card-menu/card-menu.component.ts
+++ planning-poker-pragma/src/app/components/organisms/card-menu/card-menu.component.ts
@@ -2,20 +2,37 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ScoringModeItemI } from '../../../interfaces/scoring-mode-interface';
 import { CardComponent } from '../../atoms/card/card.component';
+import { ClassroomsService } from '../../../shared/services/classrooms-service/classrooms.service';
+import { ButtonComponent } from '../../atoms/button/button.component';
 
 @Component({
   selector: 'app-card-menu',
   standalone: true,
-  imports: [CommonModule, CardComponent],
+  imports: [CommonModule, CardComponent, ButtonComponent],
   templateUrl: './card-menu.component.html',
   styleUrl: './card-menu.component.scss',
 })
 export class CardMenuComponent {
-  @Input() visualization: 'player' | 'spectator' | '' = '';
+  public scoringModeOptions: ('fibonacci' | 'oneToFive' | 'oneHundred')[] = ['fibonacci', 'oneToFive', 'oneHundred']
+  public scoringModeWindow: boolean = false;
+  public scoringMode: ScoringModeItemI[] = [];
+
   @Input() selectedCard: string = '';
-  @Input() scoringMode: ScoringModeItemI[] = [];
+  @Input() visualization: 'player' | 'spectator' | '' = '';
 
   @Output() clickEvent: EventEmitter<string> = new EventEmitter<string>();
+
+  constructor(private classroomService: ClassroomsService) {
+    this.scoringMode = this.classroomService.createScoringMode('fibonacci');
+  }
+  
+  getScoringMode(value: 'fibonacci' | 'oneToFive' | 'oneHundred'){
+    this.scoringMode = this.classroomService.createScoringMode(value);
+  }
+
+  switchDisplayModes() {
+    this.scoringModeWindow = !this.scoringModeWindow;
+  }
 
   onButtonClick(value: string) {
     this.clickEvent.emit(value);
