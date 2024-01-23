06e7c4d7c59e38d151d4aafa389f2a2960137e8d
Santiago <santib.2001@hotmail.com>
2024-01-23 00:08:47 -0500

    test[UsersService]: Added unit test for getRoom() method

--- planning-poker-pragma/src/app/shared/services/classrooms-service/classrooms.service.spec.ts
+++ planning-poker-pragma/src/app/shared/services/classrooms-service/classrooms.service.spec.ts
@@ -14,7 +14,12 @@ describe('ClassroomsService', () => {
 
   //createRoom()
   it('should create a new room with the given user', () => {
-    const user: UserInRoomI = { id: '1', username: 'user1', rol: 'player', cardSelected: '' };
+    const user: UserInRoomI = {
+      id: '1',
+      username: 'user1',
+      rol: 'player',
+      cardSelected: '',
+    };
     const roomId = 'room1';
 
     const room = service.createRoom(roomId, user);
@@ -31,4 +36,30 @@ describe('ClassroomsService', () => {
     expect(service['rooms']).toContain(room);
   });
 
+  //getRoom()
+  it('should return the room with the specified ID', () => {
+    const user: UserInRoomI = {
+      id: '1',
+      username: 'user1',
+      rol: 'player',
+      cardSelected: '',
+    };
+    const roomId = 'room1';
+
+    const room = service.createRoom(roomId, user);
+
+    const retrievedRoom = service.getRoom(roomId);
+
+    // Check if the retrieved room matches the created room
+    expect(retrievedRoom).toEqual(room);
+  });
+
+  it('should return undefined for non-existent room ID', () => {
+    const roomId = 'nonExistentRoom';
+
+    const retrievedRoom = service.getRoom(roomId);
+
+    // Check if undefined is returned for non-existent room ID
+    expect(retrievedRoom).toBeUndefined();
+  });
 });
